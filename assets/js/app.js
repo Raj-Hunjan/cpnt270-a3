b11609b67c1e9369f742d5bd9f718d6d8a35d8a5
Raj-Hunjan <rajhunjann@gmail.com>
2021-12-05 21:58:44 -0700

    It works 🎉

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 const cakes = async () => {
-  const response = await fetch('/.netlify/functions/cake');
-  const data = await response.json();
+  const response = await fetch('/.netlify/functions/cake')
+  const data = await response.json()
 
-  const cakeFigure = data.find((item) => item.caption.startsWith('#cake'));
+  const cakeFigure = data.find((item) => item.caption.startsWith('#cake'))
 
   document.querySelector('.cake').innerHTML = `
     <figure>
@@ -10,7 +10,39 @@ const cakes = async () => {
       <figcaption>${cakeFigure.caption}</figcaption>
     </figure>
   `
-  console.log(data);
+  console.log(data)
 }
 
-cakes();
+const cupCakes = async () => {
+  const response = await fetch('/.netlify/functions/cake')
+  const data = await response.json()
+
+  const cupFigure = data.find((item) => item.caption.startsWith('#cupcake'))
+
+  document.querySelector('.cupcake').innerHTML = `
+    <figure>
+      <img src="${cupFigure.url}" alt="picture of a cupcake">
+      <figcaption>${cupFigure.caption}</figcaption>
+    </figure>
+  `
+  console.log(data)
+}
+
+const donuts = async () => {
+  const response = await fetch('/.netlify/functions/cake')
+  const data = await response.json()
+
+  const donutFigure = data.find((item) => item.caption.startsWith('#cake'))
+
+  document.querySelector('.donut').innerHTML = `
+    <figure>
+      <img src="${donutFigure.url}" alt="picture of cake">
+      <figcaption>${donutFigure.caption}</figcaption>
+    </figure>
+  `
+  console.log(data)
+}
+
+cakes()
+cupCakes()
+donuts()
